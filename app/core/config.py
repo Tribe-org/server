2009fef8e114e536b8799b5c5002cc28c8bb83e2
moonformeli <mgyang95@gmail.com>
2024-09-20 07:36:17 +0900

    [TRIBE-53] refactor: 환경변수는 모두 Config 파일에서 관리

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 
 class Config:
+    APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
     DATABASE_URL = os.getenv("DATABASE_URL")
     NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
     NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
--- app/main.py
+++ app/main.py
@@ -1,42 +1,20 @@
-import os
-from enum import Enum
-
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.core import database_bootstrap
+from app.core import Config, database_bootstrap
 from app.routers.router import main_router
-
-load_dotenv()
 
 # 데이터베이스 설정
 database_bootstrap()
 
-
-class Environment(str, Enum):
-    DEV = "DEV"
-    PROD = "PROD"
-
-
-env = os.getenv("ENV")
-
-if env == Environment.DEV:
-    # 개발 환경일 때는 구글 로그인 HTTP에서 가능하도록 설정
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
 app = FastAPI()
 
-APP_SECRET_KEY = os.getenv("APP_SECRET_KEY")
-KAKAO_CLIENT_ID = os.getenv("KAKAO_CLIENT_ID")
-KAKAO_CLIENT_SECRET = os.getenv("KAKAO_CLIENT_SECRET")
-KAKAO_REDIRECT_URL = os.getenv("KAKAO_REDIRECT_URL")
-GOOGLE_REDIRECT_URL = os.getenv("GOOGLE_REDIRECT_URL")
 
 # CORS 설정 추가
 origins = ["http://localhost:8000"]
 
+# 미들웨어 설정
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -45,8 +23,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(SessionMiddleware, secret_key=APP_SECRET_KEY)
+app.add_middleware(SessionMiddleware, secret_key=Config.APP_SECRET_KEY)
 
+# 라우트 설정
 app.include_router(main_router, prefix="/v1")
 
 
