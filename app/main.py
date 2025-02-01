978562ad28968b891c03151f65c36f7573744086
Daewon Paeng <48875566+fora22@users.noreply.github.com>
2025-02-01 20:27:01 +0900

    feat. 서버 생명주기 이벤트 핸들러 추가 & database bootstrap dev 환경에서만 동작하도록 수정

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.core import Config, OpenAPI, database_bootstrap
+from app.core import Config, EnvTypes, OpenAPI, database_bootstrap
 from app.routers.router import main_router
 
-# 데이터베이스 설정
-database_bootstrap()
 
-app = FastAPI(docs_url="/api/docs")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    https://fastapi.tiangolo.com/advanced/events/#lifespan
+    FastAPI Server 생명주기를 컨트롤 하는 함수
+    """
+    # Server Start Up Event
+    if Config.ENV == EnvTypes.DEV:
+        # NOTE - meta.create_all 보다는 Alembic 을 활용하여
+        # NOTE - 버전별 스키마 마이그레이션을 하는게 좋습니다만
+        # NOTE - Alembic 을 활용하기에는 배 보다 배꼽이 크므로 일단 if 문 처리
+        database_bootstrap()  # 모든 DB 테이블 생성
+    yield
+    # Server Shut down Event
+
+
+app = FastAPI(docs_url="/api/docs", lifespan=lifespan)
+
 
 # 스웨거 설정
 app.openapi = OpenAPI(app).get_customized_openapi
